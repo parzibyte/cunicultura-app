137561fdeb4d1d30e12f44a1aa89862b7bc37213
Luis Cabrera Benito <parzibyte@gmail.com>
2022-01-17 12:37:45 -0600

    Agregando buefy

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+      <div class="columns">
+        <div class="column">
+          <section>
+            <h1 class="is-size-1">Probando Buefy</h1>
+            <button class="button is-medium" @click="mostrarToast">
+                Mostrar un toast
+            </button>
+          </section>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  methods: {
+    mostrarToast(){
+      this.$buefy.toast.open("¡Hola, Buefy! Parzibyte.me")
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
