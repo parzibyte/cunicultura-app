9ccae880b4365b21c9ae6d311f63efe09bf7df9e
Luis Cabrera Benito <parzibyte@gmail.com>
2022-01-20 18:53:40 -0600

    Acerca de

---
+++ src/components/AcercaDe.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="hero is-medium is-info">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">Acerca de</h1>
+        <img class="image" src="https://picsum.photos/200" />
+        <h2 class="subtitle">App para cunicultura</h2>
+
+        Creado y mantenido con
+        <b-icon icon="heart" type="is-danger"></b-icon>
+        por
+        <a
+          target="_blank"
+          class="has-text-warning"
+          href="https://parzibyte.me/blog"
+          >Parzibyte</a
+        >
+        usando
+        <b-icon icon="vuejs"></b-icon>
+        y
+        <b-icon icon="firebase"></b-icon>
+        <br />
+        <a
+          target="_blank"
+          class="has-text-warning"
+          href="https://parzibyte.me/blog/sigueme/"
+          >Puedes seguirme aquí</a
+        >
+        y ver
+        <a
+          class="has-text-warning"
+          href="https://parzibyte.me/blog/software-creado-por-parzibyte/"
+          target="_blank"
+          >más proyectos aquí</a
+        >
+      </div>
+    </div>
+  </section>
+</template>
