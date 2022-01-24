3d51e61d5eb7c845dbb8f522c2483f4bc9192c29
Luis Cabrera Benito <parzibyte@gmail.com>
2022-01-23 22:13:17 -0600

    Select de conejos para su reutilización

---
+++ src/components/Conejos/SelectConejos.vue
@@ -0,0 +1,66 @@
+<template>
+  <b-select
+    :loading="cargando"
+    @input="onCambiado"
+    :placeholder="placeholder()"
+  >
+    <option v-for="conejo in conejos" :value="conejo" :key="conejo.id">
+      {{ conejo.identificador }}
+    </option>
+  </b-select>
+</template>
+<script>
+import ConejosService from "../../services/ConejosService";
+export default {
+  props: ["conejoSeleccionado", "genero"],
+  data: () => ({
+    cargando: false,
+    conejos: [],
+  }),
+  async mounted() {
+    await this.obtenerConejosYEscucharCambios();
+  },
+  methods: {
+    placeholder() {
+      if (this.genero === "M") {
+        return "Seleccione un conejo";
+      } else {
+        return "Selecciona una coneja";
+      }
+    },
+    onCambiado(conejo) {
+      this.$emit("update:conejoSeleccionado", conejo);
+    },
+    indiceDeConejo(idConejo) {
+      return this.conejos.findIndex((conejo) => conejo.id === idConejo);
+    },
+    async obtenerConejosYEscucharCambios() {
+      this.cargando = true;
+      const instantanea = await ConejosService.obtenerConejosConGenero(
+        this.genero
+      );
+      instantanea.docChanges().forEach((cambio) => {
+        const conejo = cambio.doc.data();
+        const idConejo = cambio.doc.id;
+        if (cambio.type === "added") {
+          conejo.id = idConejo;
+          this.conejos.push(conejo);
+        }
+        if (cambio.type === "modified") {
+          const indice = this.indiceDeConejo(idConejo);
+          if (indice !== -1) {
+            this.$set(this.conejos, indice, conejo);
+          }
+        }
+        if (cambio.type === "removed") {
+          const indice = this.indiceDeConejo(idConejo);
+          if (indice !== -1) {
+            this.conejos.splice(indice, 1);
+          }
+        }
+      });
+      this.cargando = false;
+    },
+  },
+};
+</script>
