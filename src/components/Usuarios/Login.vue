b425e32354205e40ac526504005f2238897d62f3
Luis Cabrera Benito <parzibyte@gmail.com>
2022-01-25 15:44:40 -0600

    Login

---
+++ src/components/Usuarios/Login.vue
@@ -0,0 +1,80 @@
+<template>
+  <section class="section">
+    <div class="columns">
+      <div class="column">
+        <img class="image is-centered" :src="Pinto" alt="" />
+        <b-field label="Correo">
+          <b-input
+            v-model="correo"
+            type="email"
+            placeholder="tu@correo.aquí"
+            :loading="cargando"
+          ></b-input
+        ></b-field>
+        <b-field label="Contraseña">
+          <b-input
+            v-model="palabraSecreta"
+            type="password"
+            placeholder="Tu contraseña"
+            :loading="cargando"
+          ></b-input
+        ></b-field>
+        <b-button
+          :loading="cargando"
+          class="is-success"
+          @click="iniciarSesion()"
+          >Iniciar sesión</b-button
+        >
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import Pinto from "../../assets/pinto.jpg";
+import BaseDeDatosService from "../../services/BaseDeDatosService";
+export default {
+  data: () => ({
+    correo: "",
+    palabraSecreta: "",
+    cargando: false,
+    Pinto,
+  }),
+  methods: {
+    async iniciarSesion() {
+      if (!this.correo || !this.palabraSecreta) {
+        this.$buefy.toast.open("Completa los campos");
+        return;
+      }
+      if (!this.correo.includes("@")) {
+        // Validación muy simple, ya que Firebase se encarga de todo
+        this.$buefy.toast.open("Escribe un correo válido");
+        return;
+      }
+      this.cargando = true;
+      BaseDeDatosService.obtener(); // Inicializar app, no queremos la base de datos
+      const auth = getAuth();
+      try {
+        await signInWithEmailAndPassword(
+          auth,
+          this.correo,
+          this.palabraSecreta
+        );
+        this.$buefy.toast.open("Bienvenido");
+      } catch (error) {
+        this.$buefy.dialog.alert({
+          title: "Error",
+          message:
+            "Error iniciando sesión. Verifica tu correo y contraseña. El error es: " +
+            error.message,
+          type: "is-danger",
+          hasIcon: true,
+          icon: "alert",
+        });
+      } finally {
+        this.cargando = false;
+      }
+    },
+  },
+};
+</script>
--- src/router.js
+++ src/router.js
@@ -4,10 +4,14 @@ import Tabs from '@/components/Tabs'
 import Apareamientos from '@/components/Conejos/Apareamiento/Apareamientos'
 import AgregarApareamiento from '@/components/Conejos/Apareamiento/AgregarApareamiento'
 import EditarConejo from '@/components/Conejos/EditarConejo'
+import Login from '@/components/Usuarios/Login'
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import "./services/BaseDeDatosService";
+import BaseDeDatosService from './services/BaseDeDatosService'
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
     routes: [
         {
             path: '/',
@@ -29,6 +33,33 @@ export default new Router({
             name: 'EditarConejo',
             component: EditarConejo,
         },
+        {
+            path: '/login',
+            name: 'Login',
+            component: Login,
+        }
 
     ]
 });
+
+BaseDeDatosService.obtener(); // No queremos la base de datos, solo queremos inicializar la app
+onAuthStateChanged(getAuth(), (user) => {
+    if (!user) {
+        if (router.currentRoute.name !== "Login") {
+            router.push({ name: "Login" });
+        }
+    } else {
+
+        router.push({ name: "Tabs" });
+    }
+});
+router.beforeEach((haciaDonde, desdeDonde, siguiente) => {
+    const usuario = getAuth().currentUser;
+    if (!usuario && haciaDonde.name !== "Login") {
+        siguiente({ name: "Login" });
+    } else {
+        siguiente();
+    }
+
+});
+export default router;
