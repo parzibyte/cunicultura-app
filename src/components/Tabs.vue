846f1eef5592f32e373c68fbcbf2d6a47a41876a
Luis Cabrera Benito <parzibyte@gmail.com>
2022-01-25 18:42:07 -0600

    Sign out

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -12,6 +12,12 @@
     <b-tab-item icon="information">
       <acerca-de />
     </b-tab-item>
+    <b-tab-item icon="logout">
+      <section class="section">
+        <h3 class="is-size-3">Cerrando sesión...</h3>
+        <b-loading :is-full-page="false" :active="true"></b-loading>
+      </section>
+    </b-tab-item>
   </b-tabs>
 </template>
 <script>
@@ -19,10 +25,23 @@ import AgregarConejo from "./Conejos/AgregarConejo.vue";
 import Conejos from "./Conejos/Conejos.vue";
 import AcercaDe from "./AcercaDe.vue";
 import Ventas from "./Ventas/Ventas.vue";
+import { getAuth, signOut } from "@firebase/auth";
 export default {
   components: { AgregarConejo, Conejos, AcercaDe, Ventas },
   data: () => ({
     tabActiva: 1,
   }),
+  watch: {
+    async tabActiva() {
+      if (this.tabActiva === 4) {
+        try {
+          await signOut(getAuth());
+          this.$buefy.toast.open("Hasta pronto");
+        } catch (e) {
+          this.$buefy.toast.open("Error cerrando sesión: " + e.message);
+        }
+      }
+    },
+  },
 };
 </script>
